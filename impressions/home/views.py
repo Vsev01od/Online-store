1082ee4ba7d139e0c23afe861e2634650ae5e1e9
Vsev01od <vsevolodchel@gmail.com>
2026-08-12 18:22:41 +0300

    first commit

---
+++ impressions/home/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+import django.http
+
+def index(request):
+    return django.http.HttpResponse("<body>Hello, word</body>")
---
+++ impressions/impressions/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+import home.views
+
+urlpatterns = [
+    path('', home.views.index),
+    path("admin/", admin.site.urls),
+]
