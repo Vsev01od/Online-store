4fc5062bdf5e359638e66faf9a6dc891c1eb6595
Vsev01od <vsevolodchel@gmail.com>
2026-08-25 14:32:36 +0300

    create nice admin

--- impressions/impressions/settings.py
+++ impressions/impressions/settings.py
@@ -40,7 +40,9 @@ ROOT_URLCONF = "impressions.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -91,3 +93,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
