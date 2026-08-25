fa7a3f3aa2c2b5b0f27e548cdc6fb4d7f248e3f2
Vsev01od <vsevolodchel@gmail.com>
2026-08-25 14:15:28 +0300

    add text and slug in catalog models

--- impressions/catalog/models.py
+++ impressions/catalog/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-from django.core.validators import MinLengthValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 from catalog.validators import validators_item
 
 
 class Category(models.Model):
-    name = models.TextField(
+    name = models.CharField(
         "Название",
+        unique=True
+    )
+
+    slug = models.CharField(
+        "Слаг",
+        validators=[
+            MaxLengthValidator(150),
+        ],
+        default="",
+        blank=True,
     )
 
     class Meta:
@@ -15,7 +25,7 @@ class Category(models.Model):
 
 
 class Tag(models.Model):
-    name = models.TextField(
+    name = models.CharField(
         "Имя",
         validators=[
             MinLengthValidator(2),
@@ -25,22 +35,41 @@ class Tag(models.Model):
         unique=True,
     )
 
+    slug = models.CharField(
+        "Слаг",
+        validators=[
+            MaxLengthValidator(150),
+        ],
+        default="",
+        blank=True,
+    )
+
     class Meta:
         verbose_name = "Тег"
         verbose_name_plural = "Теги"
 
 
 class Item(models.Model):
-    name = models.TextField(
+    name = models.CharField(
         "Имя",
         validators=[
             MinLengthValidator(2),
-            validators_item,
         ],
         help_text="Имя должнобыть больше, чем из 2х букв",
     )
 
     is_published = models.BooleanField(default=False)
+
+    text = models.TextField(
+        "Описание",
+        validators=[
+            MaxLengthValidator(150),
+            validators_item,
+        ],
+        help_text="Описание должно быть не больше чем 150 букв",
+        default="",
+        blank=True,
+    )
 
     category = models.ForeignKey(
         Category,
--- impressions/catalog/validators.py
+++ impressions/catalog/validators.py
@@ -3,7 +3,7 @@ from django.core import validators
 
 # Простой способ создания своего валидатора
 def validators_item(value):
-    word = "товар"
+    word = "Описание"
     if word not in value:
         raise validators.ValidationError(f"Обязательно используйте слово: {word}")
     return value
